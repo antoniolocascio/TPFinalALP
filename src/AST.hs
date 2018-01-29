42186dfd40a056cee32c32b0160fd63749e6fce2
Antonio <antonio.locascio1@gmail.com>
2018-01-29 00:00:34 -0800

    New comments

--- src/AST.hs
+++ src/AST.hs
@@ -4,49 +4,44 @@ import Data.Vector as V
 import GHC.Int (Int32)
 import Data.List as L
 
---data Estructura = Rectangulo [Estructura] | Circulo Bool Double | Nada 
+
+-- Tipo de datos utilizado para describir la estructura de un documento escaneado
 type Structure = [StructPage]
 data StructPage = Rectangle  [StructPage] | Circle Bool
-
-type Punto = (Int32, Int32)
-data Contorno = C Double (V.Vector Punto) (V.Vector Contorno)
 
 instance Show StructPage where
   show (Rectangle hijos) = "Rectangle " L.++ show hijos
   show (Circle True) = "Marked Circle"
   show (Circle False) = "Unmarked Circle"
-  --show Nada = "Nothing" 
+
+
+
+-- Tipo de datos que describe un documento
+type Document = [Page] 
+data Page = Section Title Subsection deriving Show
+data Subsection = Subs [Page] | Options Restriction [Option] deriving Show
+
+type Restriction = Bool 
+type Option = String 
+type Title = String
+
+
+
+-- Tipos de datos utilizados para simplificar operaciones de OpenCV 
+type Punto = (Int32, Int32)
+data Contorno = C Double (V.Vector Punto) (V.Vector Contorno)
 
 instance Show Contorno where
   show (C a puntos hijos) = "C: " L.++ "A: " L.++ show a L.++ " " L.++ show puntos L.++ "\n Hijos: " L.++ show hijos 
 
 
 
--- data Document = Section Title Subsection deriving Show
--- data Subsection = Subs [Document] | Options Restriction [Option] deriving Show
-
-
-type Document = [Page] 
-data Page = Section Title Subsection deriving Show
-data Subsection = Subs [Page] | Options Restriction [Option] deriving Show
-
-type Restriction = Bool --True -> solo uno
-type Option = String 
-type Title = String
-
+-- Representacion de resultados
 data Result = Sect [Result] | Ans [Res] deriving Show
 type Res = Bool
 
-type Error = String
-
-raise :: String -> Either Error a
-raise e = Left e
-
-type ErrorResult = Either Error Result
-
-
-type SectNum = [Int]
 type FlatResult = [(SectNum, [Int])] 
+type SectNum = [Int]
 
 showFlatResult :: FlatResult -> String
 showFlatResult fr = "Results:\n" L.++ showFlatResult' fr
@@ -54,3 +49,10 @@ showFlatResult fr = "Results:\n" L.++ showFlatResult' fr
     showFlatResult' [] = ""
     showFlatResult' ((sn, res):fr) = "Question " L.++ showDottedSN sn L.++ ": " L.++ show res L.++ "\n" L.++ showFlatResult' fr
     showDottedSN = (L.intersperse '.') . L.concat . (L.map show) . L.reverse 
+
+type Error = String
+
+raise :: String -> Either Error a
+raise e = Left e
+
+type ErrorResult = Either Error Result
