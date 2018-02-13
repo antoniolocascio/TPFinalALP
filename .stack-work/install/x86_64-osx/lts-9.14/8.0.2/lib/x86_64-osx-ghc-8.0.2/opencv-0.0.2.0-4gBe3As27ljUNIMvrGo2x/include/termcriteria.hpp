a0f4e63dc518e16ce37e9bf97fafbf5c246058a5
Antonio <antonio.locascio1@gmail.com>
2018-02-13 15:14:38 -0300

    removed .stack-work/

--- .stack-work/install/x86_64-osx/lts-9.14/8.0.2/lib/x86_64-osx-ghc-8.0.2/opencv-0.0.2.0-4gBe3As27ljUNIMvrGo2x/include/termcriteria.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __THEA_TERMCRITERIA_H__
-#define __THEA_TERMCRITERIA_H__
-
-#define TERMCRITERIA_COUNT TermCriteria::COUNT
-#define TERMCRITERIA_EPS   TermCriteria::EPS
-
-#endif /* __THEA_TERMCRITERIA_H__ */
